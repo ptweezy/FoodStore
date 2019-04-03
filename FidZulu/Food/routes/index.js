9ed377ea9abd491b5e03898b7d3ba73379aa3f77
Parker Loflin <parker.loflin@fmr.com>
2019-04-03 14:32:07 -0400

    initial commit

--- FidZulu/Food/routes/index.js
+++ FidZulu/Food/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var foods = require('../modules/foods');
 var url = require('url');
+var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -10,7 +11,7 @@ router.get('/', function(req, res, next) {
 router.get('/food/all',
 function (request, response) {
 
-  //var get_params = url.parse(request.url, true).query;
+  response.setHeader('content-type', 'application/json');
   var get_all = foods.list();
 
   response.end(JSON.stringify(get_all));
@@ -52,6 +53,33 @@ function (request, response){
     response.end(JSON.stringify(obj));
 
 });
+
+router.post('/food/add', function(request, response){
+
+  response.setHeader('content-type', 'application/json');
+  var obj = foods.list();
+  var requestHeaders = request.headers;
+  var fName = requestHeaders.name;
+  var fBrand = requestHeaders.brand;
+  var fWeight = requestHeaders.weight;
+  var fCalories = requestHeaders.calories;
+  var fPrice = requestHeaders.price;
+
+  var foodToAdd = new Food(fName, fBrand, fWeight, fCalories, fPrice);
+  obj[obj.length] = foodToAdd;
+  fs.writeFileSync('../data/Foodjson.json', JSON.stringify(obj));
+
+  response.end(JSON.stringify(obj));
+  
+});
+
+function Food(fName, fBrand, fWeight, fCalories, fPrice){
+  this.name = fName,
+  this.brand = fBrand,
+  this.weight = fWeight,
+  this.calories = fCalories,
+  this.price = fPrice;
+}
 
  
 
